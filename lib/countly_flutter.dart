c2d45ea5f9579673affab95b7ee45b3be5185f40
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2021-04-19 16:44:36 +0500

    20.11.2 changes - updateSessionInterval method added - Deprecated keys issues fixed in pubspec.yaml - flutter_plugin_android_lifecycle updated to latest version (2.0.1) - Updated underlying android SDK to 20.11.6 - COUNTLY_EXCLUDE_PUSHNOTIFICATIONS flag added for iOS push.

--- lib/countly_flutter.dart
+++ lib/countly_flutter.dart
@@ -18,7 +18,11 @@ class Countly {
   /// its value should be updated from [setLoggingEnabled(bool flag)].
   static bool _isDebug = false;
 
-  static final String TAG = "CountlyFlutter";
+  /// Used to determine if init is called.
+  /// its value should be updated from [init(...)].
+  static bool _isInitialized = false;
+
+  static final String tag = "CountlyFlutter";
 
   /// Flag to determine if crash logging functionality should be enabled
   /// If false the intercepted crashes will be ignored
@@ -33,10 +37,11 @@ class Countly {
   static log(String message, {LogLevel logLevel = LogLevel.DEBUG}) async {
     String logLevelStr = describeEnum(logLevel);
     if(_isDebug){
-      print('[$TAG] ${logLevelStr}: ${message}');
+      print('[$tag] $logLevelStr: $message');
     }
   }
   static Future<String> init(String serverUrl, String appKey, [String deviceId]) async {
+    _isInitialized = true;
     if (Platform.isAndroid) {
       messagingMode = {"TEST": "2", "PRODUCTION": "0"};
     }
@@ -327,6 +332,26 @@ static Future<String> onNotification(Function callback) async {
     });
     log(result);
     return result;
+  }
+
+  /// Sets the interval for the automatic session update calls
+  /// min value 1 (1 second),
+  /// max value 600 (10 minutes)
+  /// [int sessionInterval]- delay in seconds
+  static Future<String> updateSessionInterval(int sessionInterval) async {
+    if(_isInitialized) {
+      log('updateSessionInterval should be called before init',logLevel: LogLevel.WARNING);
+      return "updateSessionInterval should be called before init";
+    }
+    List <String> args = [];
+    args.add(sessionInterval.toString());
+    log(args.toString());
+    final String result = await _channel.invokeMethod('updateSessionInterval', <String, dynamic>{
+      'data': json.encode(args)
+    });
+    log(result);
+    return result;
+
   }
 
   /// Events get grouped together and are sent either every minute or after the unsent event count reaches a threshold. By default it is 10
